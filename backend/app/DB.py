7e5bdf06580949aa3c3eb15f36c33e6f22c7d2ce
S3V04KA <sevstar2206@gmail.com>
2024-04-24 15:44:00 +0300

    qwe

--- backend/app/DB.py
+++ backend/app/DB.py
@@ -121,7 +121,7 @@ async def get_section(section_id: str):
     section['chapterIds'] = None
     # section['id'] = str(section['_id'])
     # del section['_id']
-    return SectionResponse(**section)
+    return SectionResponse(chapters=section['chapters'], chapterIds=None, id=section['id'], name=section['name'])
 
 async def find_substring_in_content(elements, substring, new_class, modified_elements=None):
     if modified_elements is None:
