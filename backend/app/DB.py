8bce9a8d619cae2027aaaa3683cbe81deb3923ba
S3V04KA <sevstar2206@gmail.com>
2024-05-21 15:22:54 +0500

    qwe

--- backend/app/DB.py
+++ backend/app/DB.py
@@ -45,6 +45,7 @@ async def init():
     name = chapters[0]['content'][0]['content'][0]['content'][0]
     if len(chapters[0]['name'].split(' ')) <= 2 and 'ГЛАВА' in name:
         for i in chapters:
+            name  = i['content'][0]['content'][0]['content'][0]
             await chapters_collection.update_one({'_id': i['_id']}, {'$set': {'name': name}})
     
 asyncio.create_task(init())
