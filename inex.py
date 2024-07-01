1d3dfe3c9509f303fa7d5b41642d24606adafe84
S3V04KA <sevstar2206@gmail.com>
2024-07-01 12:54:10 +0500

    scripts

--- inex.py
+++ inex.py
@@ -6,7 +6,7 @@ for file in glob.glob('./backend/docs/**/*.html', recursive=True):
     print(file)
     with open(f'{file}', 'r', encoding='utf-8') as f:
       html = f.read()
-    html = html.replace('<body>', """<body>
+    html = html.replace('<body style="margin: 0; padding: 10">', """<body style="margin: 0; padding: 10">
                          <div id="content" style="width: 100%">""")
     with open(f'{file}', 'w', encoding='utf-8') as f:
       f.write(html)
