2ef58d2b2f43999c8cf2ccb16a780f8777e4f79f
S3V04KA <sevstar2206@gmail.com>
2024-07-01 12:49:28 +0500

    scripts

--- inex.py
+++ inex.py
@@ -6,6 +6,7 @@ for file in glob.glob('./backend/docs/**/*.html', recursive=True):
     print(file)
     with open(f'{file}', 'r', encoding='utf-8') as f:
       html = f.read()
-    html = html.replace('<style>', '<style> mark { background-color: yellow !important; } mark.current { background-color: green !important; }')
+    html = html.replace('<body>', """<body>
+                         <div id="content" style="width: 100%">""")
     with open(f'{file}', 'w', encoding='utf-8') as f:
       f.write(html)
