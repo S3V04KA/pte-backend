71c1bec3d7e6b16d7c40a97c63e5d6938e62650a
S3V04KA <sevstar2206@gmail.com>
2024-07-01 15:51:10 +0500

    colors

--- inex.py
+++ inex.py
@@ -6,6 +6,7 @@ for file in glob.glob('./backend/docs/**/*.html', recursive=True):
     print(file)
     with open(f'{file}', 'r', encoding='utf-8') as f:
       html = f.read()
-    html = html.replace('separateWordSearch: true,', """separateWordSearch: false,""")
+    html = html.replace("""mark { background-color: yellow !important; } mark.current { background-color: green !important; }""", 
+                        """mark { background-color: var(--main-second-select-color) !important; } mark.current { background-color: var(--main-select-color) !important; }""")
     with open(f'{file}', 'w', encoding='utf-8') as f:
       f.write(html)
