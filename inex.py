99ec3ca2d758b4b18be6d7fb971182a8997cca9e
S3V04KA <sevstar2206@gmail.com>
2024-07-03 17:26:23 +0500

    image fixes

--- inex.py
+++ inex.py
@@ -6,6 +6,6 @@ for file in glob.glob('./backend/docs/**/*.html', recursive=True):
     print(file)
     with open(f'{file}', 'r', encoding='utf-8') as f:
       html = f.read()
-    html = html.replace("""src="API_URL/""", """src="/""")
+    html = html.replace("""src="images/""", """src="/images/""")
     with open(f'{file}', 'w', encoding='utf-8') as f:
       f.write(html)
