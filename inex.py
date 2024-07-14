376ee2cacf7943ac0c7bc26ccd2c98c4ba8ba241
S3V04KA <sevstar2206@gmail.com>
2024-07-15 01:02:14 +0500

    Workers

--- inex.py
+++ inex.py
@@ -6,6 +6,27 @@ for file in glob.glob('./backend/docs/**/*.html', recursive=True):
     print(file)
     with open(f'{file}', 'r', encoding='utf-8') as f:
       html = f.read()
-    html = html.replace("""src="images/""", """src="/images/""")
+    html = html.replace("""</body>""", """
+
+		<p class=MsoNormal><span lang=RU>&nbsp;</span></p>
+
+		<p class=MsoNormal><span lang=RU>&nbsp;</span></p>
+
+		<p class=MsoNormal><span lang=RU>&nbsp;</span></p>
+
+		<p class=MsoNormal><span lang=RU>&nbsp;</span></p>
+
+		<p class=MsoNormal><span lang=RU>&nbsp;</span></p>
+
+<p class=MsoNormal><span lang=RU>&nbsp;</span></p>
+
+<p class=MsoNormal><span lang=RU>&nbsp;</span></p>
+
+<p class=MsoNormal><span lang=RU>&nbsp;</span></p>
+
+<p class=MsoNormal><span lang=RU>&nbsp;</span></p>
+
+<p class=MsoNormal><span lang=RU>&nbsp;</span></p>
+</body>""")
     with open(f'{file}', 'w', encoding='utf-8') as f:
       f.write(html)
